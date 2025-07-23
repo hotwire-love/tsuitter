a2c247a68460072e382fbccefe6a5964bbcf6379
Morikiyo INOHA <morikiyo@sonicgarden.jp>
2025-07-23 20:20:19 +0900

    update: broadcast を改善
    update, destroy でも "tsueets" に broadcast されるようにした

--- app/models/tsueet.rb
+++ app/models/tsueet.rb
@@ -1,5 +1,7 @@
 class Tsueet < ApplicationRecord
-  broadcasts_refreshes
-
   validates :content, presence: true
+
+  after_create_commit -> { broadcast_refresh_later_to("tsueets") }
+  after_update_commit -> { broadcast_refresh_later; broadcast_refresh_later_to("tsueets") }
+  after_destroy_commit -> { broadcast_refresh; broadcast_refresh_later_to("tsueets") }
 end
